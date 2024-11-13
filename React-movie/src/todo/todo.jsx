2a8be1f4beae1f3f781266c6a60096a70d4fca11
ckx9798 <ckx12@Naver.com>
2024-11-13 18:31:20 +0900

    7-0 todo 기본

---
+++ React-movie/src/todo/todo.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (e) => {
+    setTodo(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(todos);
+    if (todo === "") {
+      return;
+    } else {
+      setTodos((currentArray) => [todo, ...currentArray]);
+      setTodo("");
+    }
+  };
+  return (
+    <>
+      <h1>My Todo({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write todo"
+        />
+        <button>Add To Do</button>
+      </form>
+    </>
+  );
+}
