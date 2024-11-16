1f3e959efd3dcfd297392b1e909b4611a257b439
ckx9798 <ckx12@Naver.com>
2024-11-16 22:28:28 +0900

    2-2 스타일컴포넌트 사용법

---
+++ React-masterClass/src/styledComponents/StyleComponents.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+// const BoxTwo = styled.div`
+//   background-color: tomato;
+//   width: 100px;
+//   height: 100px;
+// `;
+
+const Text = styled.div`
+  color: white;
+`;
+
+export default function StyleComponents() {
+  return (
+    <Father>
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+      {/* <BoxTwo /> */}
+      <Circle bgColor="yellow">
+        <Text>Hello</Text>
+      </Circle>
+    </Father>
+  );
+}
