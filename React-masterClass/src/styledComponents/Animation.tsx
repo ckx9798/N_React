5caec941b622d799ed985c065c7c6cc764f79b57
ckx9798 <ckx12@Naver.com>
2024-11-19 22:40:25 +0900

    2-4 rotate 애니메이션 적용

---
+++ React-masterClass/src/styledComponents/Animation.tsx
@@ -0,0 +1,45 @@
+import styled, { keyframes } from "styled-components";
+
+const rotationAnimation = keyframes`
+    0% {
+        transform: rotate(0deg);
+        border-radius: 0px;
+    }
+    50% {
+        transform: rotate(360deg);
+        border-radius: 100px;
+    }
+    100% {
+        transform: rotate(0deg);
+        border-radius: 0px;
+    }
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0%;
+    }
+  }
+`;
+
+export default function AnimationBox() {
+  return (
+    <div>
+      <Box>
+        <span>😢</span>
+      </Box>
+    </div>
+  );
+}
