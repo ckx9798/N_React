a8dc2c4200492a1b5f006c8ad886657fd1989321
ckx9798 <ckx12@Naver.com>
2024-12-07 13:01:30 +0900

    5-12 차트 데이터 연결

--- React-masterClass/src/routes/Chart.tsx
+++ React-masterClass/src/routes/Chart.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "./api";
 
 export default function Chart() {
+  const coinInfo = "btc-bitcoin";
+  const { data } = useQuery("chart", () => fetchCoinHistory(coinInfo));
   return <div>Chart</div>;
 }
--- React-masterClass/src/routes/api.ts
+++ React-masterClass/src/routes/api.ts
@@ -6,15 +6,26 @@ export async function fetchCoin() {
   return data.list;
 }
 
-export async function fetchAd(){
-  const response = await fetch("https://fandom-k-api.vercel.app/9-4/donations?pageSize=20")
+export async function fetchAd() {
+  const response = await fetch(
+    "https://fandom-k-api.vercel.app/9-4/donations?pageSize=20"
+  );
   const data = await response.json();
   return data.list;
 }
 
-export async function fetchChart(){
-  const response = await fetch("https://fandom-k-api.vercel.app/9-4/charts/{gender}?gender=female&pageSize=20")
+export async function fetchChart() {
+  const response = await fetch(
+    "https://fandom-k-api.vercel.app/9-4/charts/{gender}?gender=female&pageSize=20"
+  );
   const data = await response.json();
   return data;
 }
 
+export async function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7; // 초*분*시간*일
+  return fetch(
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+  ).then((response) => response.json());
+}
