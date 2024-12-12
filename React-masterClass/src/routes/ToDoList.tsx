2688d6c3cd627e3cc078adc2960c653c24dc7f35
ckx9798 <ckx12@Naver.com>
2024-12-12 21:22:30 +0900

    6-6-2 리액트훅폼 설치와 register, watch 활용법

--- React-masterClass/src/routes/ToDoList.tsx
+++ React-masterClass/src/routes/ToDoList.tsx
@@ -1,33 +1,51 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+// export default function ToDoList() {
+//   const [toDo, setToDo] = useState("");
+//   const [toDoError, setToDoError] = useState("");
+//   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = e;
+//     setToDoError("");
+//     setToDo(value);
+//   };
+//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     if (toDo.length < 10) {
+//       setToDoError("ToDo should be longer");
+//     }
+//     console.log(toDo);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           type="text"
+//           placeholder="할 일을 입력하세요"
+//           onChange={onChange}
+//           value={toDo}
+//         />
+//         <button>추가</button>
+//         {toDoError ? toDoError : null}
+//       </form>
+//     </div>
+//   );
+// }
 
 export default function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const [toDoError, setToDoError] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setToDoError("");
-    setToDo(value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (toDo.length < 10) {
-      setToDoError("ToDo should be longer");
-    }
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="할 일을 입력하세요"
-          onChange={onChange}
-          value={toDo}
-        />
+      <form>
+        <input {...register("email")} placeholder="email" />
+        <input {...register("firstName")} placeholder="firstName" />
+        <input {...register("lastName")} placeholder="lastName" />
+        <input {...register("username")} placeholder="username" />
+        <input {...register("password")} placeholder="password" />
+        <input {...register("passwordConfirm")} placeholder="passwordConfirm" />
         <button>추가</button>
-        {toDoError ? toDoError : null}
       </form>
     </div>
   );
